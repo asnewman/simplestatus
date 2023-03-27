cc237f6b56ffec845df4ac5de8fdb14bf9825cf4
asnewman <ashznewman@gmail.com>
2023-03-26 22:24:25 -0700

    more better

--- main.py
+++ main.py
@@ -1,28 +1,83 @@
-from rss_parser import Parser
-from requests import get
+from dotenv import load_dotenv
+load_dotenv()
+
 import sqlite3
-import email
-import pytz
-import datetime
+import requests
+import feedparser
+import os
 
-con = sqlite3.connect("data.db")
-cur = con.cursor()
+slack_webhook = os.environ.get('SLACK_WEBHOOK')
 
-rss_url = "https://www.redditstatus.com/history.rss"
-xml = get(rss_url)
+def update_rss_feeds(feeds):
+    conn = sqlite3.connect('feeds.db')
+    c = conn.cursor()
 
-# Limit feed output to 5 items
-# To disable limit simply do not provide the argument or use None
-parser = Parser(xml=xml.content, limit=5)
-feed = parser.parse()
-
-# Iteratively print feed items
-for item in feed.feed:
-    timestamp = datetime.datetime.fromtimestamp( email.utils.mktime_tz(email.utils.parsedate_tz( item.publish_date )), pytz.utc ).timestamp()
-    cur.execute(f"""
-      INSERT INTO updates VALUES ("reddit", "{item.title}", "{item.description}", {timestamp}, "reddit.com", false)
+    # Create entries table if it does not exist
+    c.execute("""
+        CREATE TABLE IF NOT EXISTS entries (
+            id INTEGER PRIMARY KEY,
+            title TEXT,
+            link TEXT,
+            pub_date TEXT,
+            feed_name TEXT,
+            feed_url TEXT
+        )
     """)
-    con.commit()      
-    print(item.title)
-    print(item.description)
-    print(datetime.datetime.fromtimestamp( email.utils.mktime_tz(email.utils.parsedate_tz( item.publish_date )), pytz.utc ).timestamp())
+
+    for name, url in feeds.items():
+        feed = feedparser.parse(url)
+        for entry in feed.entries:
+            title = entry.title
+            link = entry.link
+            pub_date = entry.published
+
+            # Check if entry already exists in the database
+            c.execute("SELECT * FROM entries WHERE title=? AND link=?", (title, link))
+            existing_entry = c.fetchone()
+
+            if existing_entry is None:
+                # Entry does not exist, insert into database
+                c.execute("INSERT INTO entries (title, link, pub_date, feed_name, feed_url) VALUES (?, ?, ?, ?, ?)", (title, link, pub_date, name, url))
+                print(f"New entry added to {name} feed: {title}")
+                send_slack_message(slack_webhook, f"Issue with {name}: {title}")
+
+    # Commit changes to the database and close the connection
+    conn.commit()
+    conn.close()
+
+def send_slack_message(webhook_url, message):
+    """
+    Sends a message to a Slack channel using a webhook URL.
+
+    Args:
+        webhook_url (str): The webhook URL for the Slack channel.
+        message (str): The message to send.
+
+    Returns:
+        None.
+    """
+    # Create the payload
+    payload = {
+        "text": message
+    }
+
+    # Send the request to the Slack API
+    response = requests.post(webhook_url, json=payload)
+
+    # Check the response status code
+    if response.status_code != 200:
+        raise ValueError("Request to Slack API failed: {}".format(response.text))
+
+
+rss_feeds = {
+    "github": "https://www.githubstatus.com/history.rss",
+    "figma": "https://status.figma.com/history.rss",
+    "slack": "https://status.slack.com/feed/rss",
+    "datadog": "https://status.datadoghq.com/history.rss",
+    "fullstory": "https://status.fullstory.com/history.rss",
+    "sentry": "https://status.sentry.io/history.rss"
+}
+
+db_file = "feeds.db"
+
+update_rss_feeds(rss_feeds)
